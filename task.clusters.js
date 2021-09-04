d9f1a3be14ad8d09dc7d9ab16225a3483fd7aed2
AsademLige <49205911+AsademLige@users.noreply.github.com>
2021-09-04 16:05:02 +0300

    Add files via upload

--- task.clusters.js
+++ task.clusters.js
@@ -1,3 +1,6 @@
+//-------------------------------------------------------------
+//Вспомогательные функции
+//-------------------------------------------------------------
 let getRandom = (min, max) => {
   return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
 }
@@ -14,50 +17,60 @@ let inArray = (initial, array) => {
   return initial.some((arrInit,inx)=>compare(arrInit,array));
 }
 
-let createField = (rows, cols) => {
+//-------------------------------------------------------------
+//Функция генератора поля
+//-------------------------------------------------------------
+let createField = (rows, cols, minNum=0, maxNum=3) => {
   let field = [];
   for (let i=0;i<rows;i++){
     field[i] = [];
     for (let j=0;j<cols;j++){
-      field[i][j] = getRandom(0,3);
+      field[i][j] = getRandom(minNum,maxNum);
     }
   }
   return field;
 }
 
-let adjacentNums = (field, i, j, curArr) => {
-  let oldLength = curArr.length;
-  let buf = [];
-  if (j+1 < field[i].length && field[i][j+1] != -1 && !inArray(curArr, [i,j+1]) && field[i][j] == field[i][j+1]){
-    curArr.push([i,j+1])
+//-------------------------------------------------------------
+//Функция поиска кластера в заданной точке
+//-------------------------------------------------------------
+let assembleCluster = (field, i, j, cluster) => {
+  let oldLength = cluster.length;
+
+  if (j+1 < field[i].length && field[i][j+1] != -1 && !inArray(cluster, [i,j+1]) && field[i][j] == field[i][j+1]){
+    cluster.push([i,j+1])
   };
-  if (j-1 >= 0 && field[i][j-1] != -1 && !inArray(curArr, [i,j-1]) && field[i][j] == field[i][j-1]){
-    curArr.push([i,j-1])
+  if (j-1 >= 0 && field[i][j-1] != -1 && !inArray(cluster, [i,j-1]) && field[i][j] == field[i][j-1]){
+    cluster.push([i,j-1])
   };
-  if (i+1 < field.length && field[i+1][j] != -1 && !inArray(curArr, [i+1,j]) &&  field[i][j] == field[i+1][j]){
-    curArr.push([i+1,j])
+  if (i+1 < field.length && field[i+1][j] != -1 && !inArray(cluster, [i+1,j]) &&  field[i][j] == field[i+1][j]){
+    cluster.push([i+1,j])
   };
-  if (i-1 >= 0 && field[i-1][j] != -1 && !inArray(curArr, [i-1,j]) && field[i][j] == field[i-1][j]){
-    curArr.push([i-1,j])
+  if (i-1 >= 0 && field[i-1][j] != -1 && !inArray(cluster, [i-1,j]) && field[i][j] == field[i-1][j]){
+    cluster.push([i-1,j])
   };
 
-  if (curArr.length > oldLength){
-    curArr.forEach(function(cur){
-      buf.concat(adjacentNums(field,cur[0], cur[1], curArr));
+  if (cluster.length > oldLength){
+    cluster.forEach(function(cur){
+      cluster.concat(assembleCluster(field,cur[0], cur[1], cluster));
     });
   }
 
-  if (curArr.length > oldLength){
-    return curArr;
-  } else return [];
+  return cluster;
 }
 
+//-------------------------------------------------------------
+// Функция 'Удаления' кластера
+//-------------------------------------------------------------
 let deleteCluster = (field, cluster) => {
   field = copyArray(field);
   cluster.forEach((point)=>field[point[0]][point[1]]=-1);
   return field;
 }
 
+//-------------------------------------------------------------
+//Функция dropNums осыпает значения на ячейки со значением -1
+//-------------------------------------------------------------
 let dropNums = (field, cluster) => {
   field = copyArray(field);
   for (i=0;i<field.length;i++){
@@ -75,6 +88,9 @@ let dropNums = (field, cluster) => {
   return field;
 }
 
+//-------------------------------------------------------------
+//Функция заполнения ячеек новыми значениями
+//-------------------------------------------------------------
 let fillEmpty = (field) => {
   for (i=0;i<field.length;i++){
     for (j=0;j<field[0].length;j++){
@@ -84,13 +100,16 @@ let fillEmpty = (field) => {
   return field;
 }
 
+//-------------------------------------------------------------
+//Функция перебора поля и возврата первого найденного кластера
+//-------------------------------------------------------------
 let findCluster = (field, minClusterSize) => {
   field = copyArray(field);
   let cluster = [];
   if (minClusterSize < 2) return;
   for (let i=0;i<field.length;i++){
     for (let j=0;j<field[i].length;j++){
-      cluster = adjacentNums(field,i,j, [[i,j]]);
+      cluster = assembleCluster(field,i,j, [[i,j]]);
       if (cluster.length >= minClusterSize) {
         return cluster;
       } else cluster = [];
@@ -99,6 +118,9 @@ let findCluster = (field, minClusterSize) => {
   return cluster;
 }
 
+//-------------------------------------------------------------
+//Функция перебора поля и возврата всех найденных кластеров
+//-------------------------------------------------------------
 let findClusters = (field, minClusterSize) => {
   field = copyArray(field);
   let clusters = [];
@@ -113,6 +135,11 @@ let findClusters = (field, minClusterSize) => {
   return clusters;
 }
 
+//-------------------------------------------------------------
+//Функция перебора поля и возврата всех найденных кластеров
+//с удалением каждого найденного кластера и заполнением пустого
+//места
+//-------------------------------------------------------------
 let findClustersLoop = (field, minClusterSize) => {
   field = copyArray(field);
   let clusters = [];
